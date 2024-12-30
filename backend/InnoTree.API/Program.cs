cc14ecf3bce0d55bbf1bc6cc07ab29fc3fef62d6
Ilya Katushenok <anotherfles@gmail.com>
2024-12-30 16:02:10 +0300

    remove comments from program.cs

--- backend/InnoTree.API/Program.cs
+++ backend/InnoTree.API/Program.cs
@@ -26,13 +26,6 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//app.UseSwagger();
-//app.UseSwaggerUI();
-//}
-
 app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
